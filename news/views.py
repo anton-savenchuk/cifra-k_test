2dc86ccd7dc03d35b8f6f021b36772155035eab1
Anton Savenchuk <Anton.Savenchuk@gmail.com>
2023-05-30 22:08:54 +0700

    News output on the main page

--- news/views.py
+++ news/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from .models import News
+
+
+class NewsListView(ListView):
+    model = News
+    template_name = "news/news_list.html"
+    context_object_name = "news"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["title"] = "Главная страница"
+
+        return context
